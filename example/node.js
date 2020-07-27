025667b4ffaf784361992d420d06cbaf44d2e35a
David Weinstein <dweinst@insitusec.com>
2020-07-27 08:45:06 -0400

    Major API changes (#13)
    * Upgrade dependencies and add semistandard
    
    * Detect if the app is enterprise by checking into the embedded.mobileprovision
    
    * Major api changes
    
    * Grammar
    
    * Address code review items
    
    Co-authored-by: Sergi Àlvarez i Capilla <pancake@nowsecure.com>

--- example/node.js
+++ example/node.js
@@ -1,10 +1,20 @@
-var fs = require('fs');
-var extract = require('..');
+const fs = require('fs');
+const extract = require('..');
+const { identifyProvisioningType } = require('../lib/provisioning');
+const { promisify } = require('util');
 
-var ipa = process.argv[2];
-var fd = fs.openSync(ipa, 'r');
+const openAsync = promisify(fs.open);
 
-extract(fd, function(err, info, raw){
-  if (err) throw err;
-  console.log(info);
+(async function main () {
+  const ipa = process.argv[2];
+  const fd = await openAsync(ipa, 'r');
+  const result = await extract(fd);
+  console.log('Info.plist', result.info);
+  console.log('embedded.mobileprovision', result.mobileprovision);
+  if (result.mobileprovision !== null) {
+    console.log('Provisioning type:', identifyProvisioningType(result.mobileprovision));
+  }
+})().catch(err => {
+  process.exitCode = 1;
+  console.error(err.stack);
 });
--- index.js
+++ index.js
@@ -1,59 +1,76 @@
-var fromFd = require('yauzl').fromFd;
-var collect = require('collect-stream');
-var bplistParse = require('bplist-parser').parseBuffer;
-var plistParse = require('plist').parse;
-var reg = require('./lib/reg');
-var once = require('once');
+const promisify = require('util').promisify;
+const fromFd = promisify(require('yauzl').fromFd);
+const collect = promisify(require('collect-stream'));
+const bplistParse = require('bplist-parser').parseBuffer;
+const plistParse = require('plist').parse;
+const reg = require('./lib/reg');
 
-var chrOpenChevron = 60;
-var chrLowercaseB = 98;
+const chrOpenChevron = 60;
+const chrLowercaseB = 98;
 
-module.exports = function(fd, cb){
-  var foundPlist = false;
-  cb = once(cb || function(){});
+module.exports = async function (fd, { autoClose = true } = {}) {
+  const zip = await fromFd(fd, { autoClose });
+  zip.openReadStreamAsync = promisify(zip.openReadStream.bind(zip));
+  zip.on('error', (err) => {
+    throw err;
+  });
 
-  fromFd(fd, function(err, zip){
-    if (err) return cb(err);
-    var onentry;
+  const findInfo = async () => {
+    const matchInfo = f => reg.info.test(f);
+    const { entry } = await findEntry(zip, matchInfo);
+    if (!entry) {
+      return null;
+    }
+    const file = await zip.openReadStreamAsync(entry);
+    const src = await collect(file);
 
-    zip.on('entry', onentry = function(entry){
-      if (!reg.test(entry.fileName)) {
-        return
-      } else {
-        foundPlist = true
-      }
+    let parsed;
+    if (src[0] === chrOpenChevron) {
+      parsed = plistParse(src.toString());
+    } else if (src[0] === chrLowercaseB) {
+      parsed = bplistParse(src);
+    } else {
+      throw new Error(`unknown plist type byte (0x${src[0].toString(16)})`);
+    }
 
-      zip.removeListener('entry', onentry);
-      zip.openReadStream(entry, function(err, file){
-        if (err) return cb(err);
+    return parsed;
+  }
 
-        collect(file, function(err, src){
-          if (err) return cb(err);
+  const findProv = async () => {
+    const matchProv = f => reg.mobileprovision.test(f);
+    const { entry } = await findEntry(zip, matchProv);
+    if (!entry) {
+      return null;
+    }
+    const file = await zip.openReadStreamAsync(entry);
+    return collect(file);
+  };
 
-          var obj;
-          try {
-            if (src[0] === chrOpenChevron) {
-              obj = plistParse(src.toString());
-            } else if (src[0] === chrLowercaseB) {
-              obj = bplistParse(src);
-            } else {
-              return cb(new Error('unknown plist type %s', src[0]));
-            }
-          } catch (err) {
-            return cb(err);
-          }
+  const [info, mobileprovision] = await Promise.all([findInfo(), findProv()]);
+  return {
+    info,
+    mobileprovision
+  };
+};
 
-          cb(null, [].concat(obj), src);
-        });
-      });
-    });
-
+function findEntry (zip, match) {
+  return new Promise((resolve, reject) => {
+    let found = false;
+    let onentry;
     zip
-    .on('end', function() {
-      if (!foundPlist) { return cb(new Error('No Info.plist found')); }
-    })
-    .on('error', function(err) {
-      return cb(err);
-    });
+      .on('entry', onentry = async (entry) => {
+        try {
+          if (!match(entry.fileName)) { return; }
+          found = true;
+          zip.removeListener('entry', onentry);
+          return resolve({ entry });
+        } catch (err) {
+          return reject(err);
+        }
+      })
+      .on('end', () => {
+        const entry = null;
+        if (!found) { return resolve({ entry }); }
+      });
   });
 }
